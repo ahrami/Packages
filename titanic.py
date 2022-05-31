bc34db9480ae8c1d797927ceef3e6feebc26020c
ahrami <73282986+ahrami@users.noreply.github.com>
2022-05-31 13:32:16 +0300

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -41,4 +41,12 @@ genders.plot.bar()
 plt.xticks(rotation=60, fontsize=10)
 plt.title('genders dead', fontsize=10)
 
+fig, axs = plt.subplots(1, 1)
+axs.remove()
+genders = [0] * 2
+genders[0] = df['Sex'].where(df['Survived'] == 0).value_counts().sort_index()
+genders[1] = df['Sex'].where(df['Survived'] == 1).value_counts().sort_index()
+plt.table(cellText=genders, loc = 'center', rowLabels = ['male','female'], colLabels = ['alive','dead'])
+
+
 plt.show()
